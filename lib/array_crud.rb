5d5dd8b28c23a03f701abe7495914935dbcc0e7b
Alex Foreman <foreman.alexander@gmail.com>
2020-06-13 17:32:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push(array, "tortoise")
+  add_element_to_end_of_array.push(array, "tortoise")
 end
 
 def add_element_to_start_of_array(array, element)
